21e734d3772f157e78aac91d6269a2a043e71548
brijeshagal <brijeshagal@gmail.com>
2025-01-06 18:33:36 +0530

    adding erc20 from openzeppelin

--- autoswap-foundry/src/Autoswapper.sol
+++ autoswap-foundry/src/Autoswapper.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import {UniswapRouterV3} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract AutoSwapper {
     modifier autoSwap(
+        bytes path,
         address receiveToken,
         uint256 receiveAmount,
         address sendToken
     ) {
-        uint256 inputAmount = UniswapRouterV3().exactOutput();
+        if (sendToken == receiveToken) {
+
+        } else {
+            uint256 inputAmount = UniswapRouterV3().exactOutput();
+        }
         _;
     }
 }
